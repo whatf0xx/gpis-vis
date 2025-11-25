82eef9870127718e030b05ab4df877152b440343
whatf0xx <hfox022@gmail.com>
2025-11-25 08:38:37 +0100

    Before we do anything crazy

--- view.py
+++ view.py
@@ -2,6 +2,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_pickle("data/england_prices_wages.pkl")
-cols = ["Barley (bushel)", "Cheese (lb.)", "Wage, farm (day)", "Wage, craft (day)"]
+cols = [
+    "Barley (bushel)",
+    "Cheese (lb.)",
+    "Cider (gallon)",
+    "Linen cloth (yard)",
+    "Oats (bushel)",
+    "Peas (bushel)",
+    "Wage, farm (day)",
+    "Wage, craft (day)",
+    ]
 df[cols].plot(logy=True)
+plt.title("Historical wages and prices in England")
 plt.show()
+# plt.savefig("england-wages.png")
